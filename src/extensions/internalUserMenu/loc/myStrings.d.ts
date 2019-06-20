df5ace15a86218d5f60eea10d4113e812ffaad99
Velin Georgiev <velin.georgiev@gmail.com>
2019-06-20 20:50:45 +0000

    init

---
+++ src/extensions/internalUserMenu/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IInternalUserMenuApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'InternalUserMenuApplicationCustomizerStrings' {
+  const strings: IInternalUserMenuApplicationCustomizerStrings;
+  export = strings;
+}
